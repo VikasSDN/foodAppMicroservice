961556f2cd0fc5e79b3dcc389ca3de3d67fcaee5
Vikas Sharma <vikas.sharma@smartdatainc.net>
2025-04-21 17:24:50 +0530

    Removed Swagger from API gateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -13,28 +13,9 @@ builder.Host.UseSerilog();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services.AddControllers();
-
-builder.Services.AddOpenApi();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Title = "API Gateway",
-        Version = "v1",
-        Description = "A simple example of a microserices API Gateway",
-    });
-});
-
 var app = builder.Build();
 
 app.MapReverseProxy();
-
-app.UseSwagger(); // Serve Swagger documentation
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
-});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,7 +26,5 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
